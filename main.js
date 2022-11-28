6c7806c49741a5661bf52d0ef10528b3e659c01d
GabiLivi <livigabriel89@gmail.com>
2022-11-28 10:02:28 -0300

    algunos cambios en el hero

---
+++ main.js
@@ -0,0 +1,11 @@
+import {slider} from "/scripts/slider.js";
+import { slider2 } from "/scripts/slider.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", function(e){
+    slider();
+    slider2();
+})
+
+
